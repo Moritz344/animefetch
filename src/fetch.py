c9de71199c34c8579260c6ad3d7576e6eeca9ac0
Moritz <157284909+Moritz344@users.noreply.github.com>
2024-10-09 19:16:07 +0200

    Update fetch.py

--- src/fetch.py
+++ src/fetch.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
-import subprocess
 import platform
 import psutil
-import cpuinfo
-import pwd
 import os
-import getpass
-import GPUtil
-import math
-from termcolor import colored,cprint
+from termcolor import colored, cprint
 import sys
 import time
 
@@ -19,31 +13,41 @@ def loading_animation(text):
         time.sleep(0.0001)
     print()
 
-
 class SystemInfo():
     def __init__(self):
-        global ascii_art 
+        global ascii_art
 
         os_info = platform.system()
-        os_info = colored(os_info,"green")
+        os_info = colored(os_info, "green")
+
+        if "Linux" in os_info:
+            linux_icon = ("🐧")
+        else:
+            linux_icon = ("💀")
+
         kernel = platform.release()
-        kernel = colored(kernel,"yellow")
+        kernel = colored(kernel, "yellow")
         shell = os.environ.get("SHELL")
-        shell = colored(shell,"red")
+        shell = colored(shell, "red")
         terminal = os.environ.get("TERM")
-        terminal = colored(terminal,"blue")
+        terminal = colored(terminal, "blue")
         CPU = platform.processor()
-        CPU = colored(CPU,"red")
+        CPU = colored(CPU, "red")
+
+        whoami = os.environ.get('USERNAME') or os.environ.get('USER')
+        whoami = colored(whoami, "red")
 
         memory_info = psutil.virtual_memory()
         memory = memory_info.total
 
         value = memory / (2 ** 20)
         value_rounded = round(value)
-        value_rounded = colored(value_rounded,"yellow")
+        value_rounded = colored(value_rounded, "yellow")
 
         self.ascii_art = f"""\
-        ⢕⢕⢕⢕⢕⠅⢗⢕⠕⣠⠄⣗⢕⢕⠕⢕⢕⢕⠕⢠⣿⠐⢕⢕⢕⠑⢕⢕⠵⢕ OS        {os_info}
+                                        {whoami}
+                                        --------
+        ⢕⢕⢕⢕⢕⠅⢗⢕⠕⣠⠄⣗⢕⢕⠕⢕⢕⢕⠕⢠⣿⠐⢕⢕⢕⠑⢕⢕⠵⢕ OS        {os_info} {linux_icon}
         ⢕⢕⢕⢕⠁⢜⠕⢁⣴⣿⡇⢓⢕⢵⢐⢕⢕⠕⢁⣾⢿⣧⠑⢕⢕⠄⢑⢕⠅⢕ Kernel    {kernel}
         ⢕⢕⠵⢁⠔⢁⣤⣤⣶⣶⣶⡐⣕⢽⠐⢕⠕⣡⣾⣶⣶⣶⣤⡁⢓⢕⠄⢑⢅⢑ Shell     {shell}
         ⠍⣧⠄⣶⣾⣿⣿⣿⣿⣿⣿⣷⣔⢕⢄⢡⣾⣿⣿⣿⣿⣿⣿⣿⣦⡑⢕⢤⠱⢐ Terminal  {terminal}
@@ -51,19 +55,16 @@ class SystemInfo():
         ⣔⢕⢥⢻⣿⡀⠈⠛⠛⠁⢠⣿⣿⣿⣿⣿⣿⣿⣿⡀⠈⠛⠛⠁⠄⣼⣿⣿⡇⢔ Memory    {value_rounded} MiB
         ⢕⢕⢽⢸⢟⢟⢖⢖⢤⣶⡟⢻⣿⡿⠻⣿⣿⡟⢀⣿⣦⢤⢤⢔⢞⢿⢿⣿⠁⢕
         ⢕⢕⠅⣐⢕⢕⢕⢕⢕⣿⣿⡄⠛⢀⣦⠈⠛⢀⣼⣿⢗⢕⢕⢕⢕⢕⢕⡏⣘⢕
-        ⢕⢕⠅⢓⣕⣕⣕⣕⣵⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣕⢕⢕⢕⢕⡵⢀⣿⣿⣘
+        ⢕⢕⠅⢓⣕⣕⣕⣕⣵⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣕⢕⢕⢕⢕⡵⢕⣿⣘
         ⢑⢕⠃⡈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢃⢕
         """
 
-    
     def loading(self):
         loading_animation(self.ascii_art)
-
 
 if __name__ == "__main__":
     system = SystemInfo()
     system.loading()
-
 
 
 
